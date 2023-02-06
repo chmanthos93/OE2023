ea57794d81fdaeb30b74341d0e9c1c0f7aa57acb
chmanthos93 <61712851+chmanthos93@users.noreply.github.com>
2023-02-07 01:30:28 +0200

    07/02/23 01.30

---
+++ Liveonair/obj/Docker/publish/wwwroot/js/protection.js
@@ -0,0 +1,11 @@
+﻿$
+    var password = "oE2023@#";
+    var x = prompt("Enter in the password ", " ");
+    if (x == password) {
+        alert("You've entered in the right password");
+    document.getElementById("safepage").style.display = "flex";
+    }
+    else {
+        window.location = "./boehringer";
+    }
+;
